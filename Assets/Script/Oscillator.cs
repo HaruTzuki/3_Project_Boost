2f7ced0b7c118de7396e210a21c137fc633343dd
HaruTzuki <dimitris.v.91@hotmail.com>
2019-06-17 17:06:53 +0300

    Add manual movement multiply with a vector3

--- Assets/Script/Oscillator.cs
+++ Assets/Script/Oscillator.cs
@@ -2,15 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[DisallowMultipleComponent]
 public class Oscillator : MonoBehaviour {
+
+    [SerializeField] Vector3 MovementVector;
+
+    //todo remove from inspector later
+    [Range(0,1)]
+    [SerializeField] float movementFactor; // 0 for not moved, 1 for fully moved.
+
+    Vector3 startingPos;
 
 	// Use this for initialization
 	void Start () {
-		
+        startingPos = this.transform.position; //Taking position from object
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        Vector3 offset = MovementVector * movementFactor;
+        this.transform.position = startingPos + offset;
 	}
 }
