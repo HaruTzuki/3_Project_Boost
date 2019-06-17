69a49102b02ba6ff8e0290e19ae0c1d2a501c3ab
HaruTzuki <dimitris.v.91@hotmail.com>
2019-06-17 10:45:01 +0300

    Adding Rotating Left and Right and added and sound effect when space pressed

--- Assets/Script/Rocket.cs
+++ Assets/Script/Rocket.cs
@@ -7,11 +7,13 @@ public class Rocket : MonoBehaviour {
 
     //Unity Components
     private Rigidbody _Rigidbody;
+    private AudioSource _AudioSource;
 
 
 	// Use this for initialization
 	void Start () {
         this._Rigidbody = GetComponent<Rigidbody>(); //Taking the reference of object Rigidbody.That inhireted from UnityEngine library.
+        this._AudioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -25,16 +27,34 @@ public class Rocket : MonoBehaviour {
         {
             //print("Thrusting");
             this._Rigidbody.AddRelativeForce(Vector3.up);
+            if(!this._AudioSource.isPlaying)
+                this._AudioSource.Play();
         }
+        else
+        {
+            if (this._AudioSource.isPlaying)
+                this._AudioSource.Stop();
+        }
+
+        //if(Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    this._AudioSource.Play();
+        //}
+        //else
+        //{
+        //    this._AudioSource.Stop();
+        //}
 
         //In this case can accept only one rotate at the same time.
         if(Input.GetKey(KeyCode.A))
         {
-            print("Rotating Left");
+            //print("Rotating Left");
+            transform.Rotate(Vector3.forward);
         }
         else if(Input.GetKey(KeyCode.D))
         {
-            print("Rotating Right");
+            // print("Rotating Right");
+            transform.Rotate(-Vector3.forward);
         }
     }
 }
