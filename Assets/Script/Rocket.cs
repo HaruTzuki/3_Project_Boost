3edafa95f7b6563413115589729d584a10f8a07e
HaruTzuki <dimitris.v.91@hotmail.com>
2019-06-17 10:51:16 +0300

    Refactoring and Cleanup code

--- Assets/Script/Rocket.cs
+++ Assets/Script/Rocket.cs
@@ -3,58 +3,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rocket : MonoBehaviour {
+public class Rocket : MonoBehaviour
+{
 
     //Unity Components
     private Rigidbody _Rigidbody;
     private AudioSource _AudioSource;
 
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         this._Rigidbody = GetComponent<Rigidbody>(); //Taking the reference of object Rigidbody.That inhireted from UnityEngine library.
         this._AudioSource = GetComponent<AudioSource>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        ProcessInput();
-	}
+    }
 
-    private void ProcessInput()
+    // Update is called once per frame
+    void Update()
     {
-        if(Input.GetKey(KeyCode.Space)) // Can thrust while rotating
+        Thrust();
+        Rotate();
+    }
+
+    private void Rotate()
+    {
+        //In this case can accept only one rotate at the same time.
+        if (Input.GetKey(KeyCode.A))
+        {
+            //print("Rotating Left");
+            transform.Rotate(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            // print("Rotating Right");
+            transform.Rotate(-Vector3.forward);
+        }
+    }
+
+    private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space)) // Can thrust while rotating
         {
             //print("Thrusting");
             this._Rigidbody.AddRelativeForce(Vector3.up);
-            if(!this._AudioSource.isPlaying)
+            if (!this._AudioSource.isPlaying)
                 this._AudioSource.Play();
         }
         else
         {
             if (this._AudioSource.isPlaying)
                 this._AudioSource.Stop();
-        }
-
-        //if(Input.GetKeyDown(KeyCode.Space))
-        //{
-        //    this._AudioSource.Play();
-        //}
-        //else
-        //{
-        //    this._AudioSource.Stop();
-        //}
-
-        //In this case can accept only one rotate at the same time.
-        if(Input.GetKey(KeyCode.A))
-        {
-            //print("Rotating Left");
-            transform.Rotate(Vector3.forward);
-        }
-        else if(Input.GetKey(KeyCode.D))
-        {
-            // print("Rotating Right");
-            transform.Rotate(-Vector3.forward);
         }
     }
 }
